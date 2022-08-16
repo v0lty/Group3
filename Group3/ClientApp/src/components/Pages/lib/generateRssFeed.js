0b5da1576292e36c8b5acc5a9c647d016ff68b86
minkinen <minkinen@yahoo.com>
2022-08-16 20:58:32 +0200

    added the unfinished generateRssFeed.js code

--- Group3/ClientApp/src/components/Pages/lib/generateRssFeed.js
+++ Group3/ClientApp/src/components/Pages/lib/generateRssFeed.js
@@ -1,1 +1,29 @@
-﻿
+﻿import { writeFileSync } from "fs";
+import RSS from "rss";
+import API from "../API";
+
+export default async function getRSS() {
+    const siteURL = "http://localhost:13021";
+    const allNews = API.getAllPosts();
+
+    const feed = new RSS({
+        title: "Software development communnity",
+        description: "A forum for a software development community",
+        site_url: siteURL,
+        feed_url: `${siteURL}/feed.xml`,
+        language: "en",
+        pubDate: new Date(),
+        copyright: `All rights reserved ${new Date().getFullYear()}, My Name`,
+    });
+
+    allNews.map((post) => {
+        feed.item({
+            title: post.title,
+            url: `${siteURL}/blogs/${post.slug}`,
+            date: post.date,
+            description: post.excerpt,
+        });
+    });
+
+    writeFileSync("./public/feed.xml", feed.xml({ indent: true }));
+}
